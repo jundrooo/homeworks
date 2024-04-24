c0071c96708d365811ec64adfbaa1771ad72368c
Javohir <166276882+jundrooo@users.noreply.github.com>
2024-04-24 15:53:07 +0500

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from foydalanuvchi import Foydalanuvchi
-
-class Admin(Foydalanuvchi):
-    def ban_user(self, user):
-        print(f"{user.username} bloklandi")
-
-    def get_user_info(self, user):
-        print(f"Username: {user.username}")
-        print(f"Email: {user.email}")
