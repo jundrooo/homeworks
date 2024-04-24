8e558d02fe179b726a4c755be9342d47d48606d3
Javohir <166276882+jundrooo@users.noreply.github.com>
2024-04-24 15:52:57 +0500

    Delete foydalanuvchi.py

--- foydalanuvchi.py
+++
@@ -1,8 +0,0 @@
-class Foydalanuvchi:
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def display_info(self):
-        print(f"Username: {self.username}")
-        print(f"Email: {self.email}")
