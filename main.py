0459bd7c81000df6321cc5e24bd5368c51cb6938
Javohir <166276882+jundrooo@users.noreply.github.com>
2024-04-24 15:52:50 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from talaba import Talaba
-from admin import Admin
-from foydalanuvchi import Foydalanuvchi
-
-admin = Admin("admin123", "admin@example.com")
-foydalanuvchi = Foydalanuvchi("foydalanuvchi1", "user1@example.com")
-
-admin.get_user_info(foydalanuvchi)
-admin.ban_user(foydalanuvchi)
