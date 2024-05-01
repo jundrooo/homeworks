7018187e619fba3b00b20ec726fdaa8c677f2820
Javohir <166276882+jundrooo@users.noreply.github.com>
2024-05-01 17:32:20 +0500

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,31 @@
+
+import json
+#1
+data = {"Model": "Malibu", "Rang": "Qora", "Yil": 2020, "Narh": 40000}
+data_json = json.dumps(data)
+with open('data_json.json','w') as f:
+    json.dump(data_json,f)
+    data_json.replace(" \\ ",'')
+
+print(data_json)
+#2
+talaba_json = """{"ism":"Hasan","familiya":"Husanov","tyil":2000}"""
+talaba_json2 = json.loads(talaba_json)
+
+print(f"Talbning ismi {talaba_json2['ism']}, Familiyasi {talaba_json2['familiya']}")
+#2.2
+
+talaba_json = """{"ism":"Hasan","familiya":"Husanov","tyil":2000}"""
+talaba_json2_2 = json.loads(talaba_json)
+
+ism  = talaba_json2_2['ism']
+familiya = talaba_json2_2['familiya']
+
+
+print(f"Ism-> {ism}, Familiya ->{familiya}")
+
+with open('ism.json','w') as file1:
+    json.dump(ism,file1)
+
+with open('familiya.json','w') as file2:
+    json.dump(familiya,file2)
